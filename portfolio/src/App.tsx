131575ad96e76532fb2886f91d85f897a757b5aa
Joshua Sy <z5259375@unsw.edu.au>
2024-08-02 02:50:56 +1000

    commented out Navbar

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,6 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
 import Name from './sections/Name/Name.tsx';
 import AboutMe from './sections/AboutMe/AboutMe';
